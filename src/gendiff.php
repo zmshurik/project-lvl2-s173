5f700614535da204b0b077892c006633030a58d1
Alexander Kurakov <zmshurik@yandex.ru>
2018-01-01 12:38:04 +0700

    add gendiff help

---
+++ src/gendiff.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Gendiff;
+
+define(
+    HELP_MESSAGE,
+    <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  --format <fmt>                Report format [default: pretty]
+
+DOC
+);
+
+function run()
+{
+    $args = Docopt::handle(HELP_MESSAGE);
+}
