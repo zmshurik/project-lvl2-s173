166cbf7f5e2b85cb0fbbc8030ee0238ed141f352
Alexander Kurakov <zmshurik@yandex.ru>
2018-01-02 15:38:23 +0700

    add diff genegator

--- src/gendiff.php
+++ src/gendiff.php
@@ -2,7 +2,60 @@
 
 namespace Differ\Gendiff;
 
-function genDiff()
+use function Differ\FileParser\parse;
+
+function getDiffAst($data1, $data2)
 {
-    return 'test work';
+    $getType = function ($key) use ($data1, $data2) {
+        if (array_key_exists($key, $data1) && array_key_exists($key, $data2)) {
+            return $data1[$key] == $data2[$key] ? 'not changed' : 'changed';
+        }
+        return array_key_exists($key, $data1) ? 'deleted' : 'added';
+    };
+    $getItem = function ($key) use ($data1, $data2, $getType) {
+        $item['name'] = $key;
+        $type = $getType($key);
+        $item['value'] = $type == 'added' ? $data2[$key] : $data1[$key];
+        $item['type'] = $type;
+        if ($type == 'changed') {
+            $item['newValue'] = $data2[$key];
+        }
+        return $item;
+    };
+    $unionKeys = \Funct\Collection\union(array_keys($data1), array_keys($data2));
+    return array_reduce($unionKeys, function ($acc, $key) use ($getItem) {
+        $acc[] = $getItem($key);
+        return $acc;
+    }, []);
+}
+
+function parseAst($ast)
+{
+    $signMap = [
+        'not changed' => ' ',
+        'changed' => '-',
+        'deleted' => '-',
+        'added' => '+'
+    ];
+    $toBoolStr = function ($value) {
+        return $value ? 'true' : 'false';
+    };
+    $parts = array_reduce($ast, function ($acc, $item) use ($signMap, $toBoolStr) {
+        extract($item);
+        $sign = $signMap[$type];
+        if ($type == 'changed') {
+            $acc[] = "+ $name: " . (is_bool($newValue) ? $toBoolStr($newValue) : $newValue);
+        }
+        $acc[] = "$sign $name: " . (is_bool($value) ? $toBoolStr($value) : $value);
+        return $acc;
+    }, ['{']);
+    return implode(PHP_EOL . '  ', $parts) . PHP_EOL . '}';
+}
+
+function genDiff($format, $file1, $file2)
+{
+    $data1 = parse($file1);
+    $data2 = parse($file2);
+    $diffAst = getDiffAst($data1, $data2);
+    return parseAst($diffAst);
 }
--- tests/gendiffTest.php
+++ tests/gendiffTest.php
@@ -4,12 +4,12 @@ namespace Differ\tests\GetdiffTest;
 
 use PHPUnit\Framework\TestCase;
 use function \Differ\Gendiff\genDiff;
+use function \Differ\Gendiff\getDiffAst;
+use function \Differ\Gendiff\parseAst;
 
 final class GendiffTest extends TestCase
 {
-    public function testGenDiff()
-    {
-        $expected = <<<DOC
+    private $result = <<<DOC
 {
     host: hexlet.io
   + timeout: 20
@@ -18,8 +18,53 @@ final class GendiffTest extends TestCase
   + verbose: true
 }
 DOC;
+    private $ast = [
+        [
+            'name' => 'host',
+            'value' => 'hexlet.io',
+            'type' => 'not changed'
+        ],
+        [
+            'name' => 'timeout',
+            'value' => 50,
+            'type' => 'changed',
+            'newValue' => 20
+        ],
+        [
+            'name' => 'proxy',
+            'value' => '123.234.53.22',
+            'type' => 'deleted',
+        ],
+        [
+            'name' => 'verbose',
+            'value' => true,
+            'type' => 'added'
+        ]
+    ];
+    public function testGenDiff()
+    {
+
         $path1 = __DIR__ . '/files/before.json';
         $path2 = __DIR__ . '/files/after.json';
-        $this->assertEquals($expected, genDiff('pretty', $path1, $path2));
+        $this->assertEquals($this->result, genDiff('pretty', $path1, $path2));
+    }
+
+    public function testGetDiffAst()
+    {
+        $data1 = [
+            "host" => "hexlet.io",
+            "timeout" => 50,
+            "proxy" => '123.234.53.22'
+        ];
+        $data2 = [
+            "timeout" => 20,
+            "verbose" => true,
+            "host" => "hexlet.io"
+        ];
+        $this->assertEquals($this->ast, getDiffAst($data1, $data2));
+    }
+    public function testParseAst()
+    {
+        $this->assertEquals($this->result, parseAst($this->ast));
     }
 }
