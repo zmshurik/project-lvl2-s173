0198a07958e1d7a70bf1267ec5790bb7fa0e551e
Alexander Kurakov <zmshurik@yandex.ru>
2018-01-04 13:11:32 +0700

    add complex parser test

--- tests/parserTest.php
+++ tests/parserTest.php
@@ -7,7 +7,7 @@ use function \Differ\Parser\parse;
 
 final class FileParserTest extends TestCase
 {
-    public function testFileParser()
+    public function testParser()
     {
         $expected = [
             "timeout" => 20,
@@ -18,6 +18,32 @@ final class FileParserTest extends TestCase
         $jsonContent = file_get_contents($jsonPath);
         $this->assertEquals($expected, parse($jsonContent, 'json'));
         $ymlPath = __DIR__ . '/fixtures/after.yml';
+        $ymlContent = file_get_contents($ymlPath);
+        $this->assertEquals($expected, parse($ymlContent, 'yml'));
+    }
+    public function testParserNested()
+    {
+        $expected = [
+            "common" => [
+                'setting1' => 'Value 1',
+                'setting3' => true,
+                'setting4' => 'blah blah',
+                'setting5' => [
+                    'key5' => 'value5'
+                ],
+            ],
+            "group1" => [
+                'foo' => 'bar',
+                'baz' => 'bars'
+            ],
+            "group3" => [
+                'fee' => 100500
+            ]
+        ];
+        $jsonPath = __DIR__ . '/fixtures/afterComplex.json';
+        $jsonContent = file_get_contents($jsonPath);
+        $this->assertEquals($expected, parse($jsonContent, 'json'));
+        $ymlPath = __DIR__ . '/fixtures/afterComplex.yml';
         $ymlContent = file_get_contents($ymlPath);
         $this->assertEquals($expected, parse($ymlContent, 'yml'));
     }
