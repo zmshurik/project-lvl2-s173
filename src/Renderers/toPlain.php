e9295173ecc15a29bceaebfa4a578615b45c4b63
Alexander Kurakov <zmshurik@yandex.ru>
2018-01-06 18:42:20 +0700

    change toPlain

--- src/Renderers/toPlain.php
+++ src/Renderers/toPlain.php
@@ -4,9 +4,17 @@ namespace Differ\Renderers\ToPlain;
 
 use function Differ\Renderers\Renderer\toBoolStr;
 
+function valueToString($value)
+{
+    if (is_array($value)) {
+        return 'complex value';
+    }
+    return is_bool($value) ? toBoolStr($value) : $value;
+}
+
 function renderToPlain($ast, $parents = '')
 {
-    $needNodes = array_filter($ast, function ($node) {
+    $neededNodes = array_filter($ast, function ($node) {
         return $node['type'] != 'not changed';
     });
     $typeMap = [
@@ -16,22 +24,21 @@ function renderToPlain($ast, $parents = '')
         },
         'added' => function ($astItem) use ($parents) {
             $fullPropertyName = $parents . $astItem['name'];
-            $value = is_array($astItem['newValue']) ? 'complex value' : $astItem['newValue'];
-            return ["Property '$fullPropertyName' was added with value: '" .
-             (is_bool($value) ? toBoolStr($value) : $value) . "'"];
+            $value = valueToString($astItem['newValue']);
+            return ["Property '$fullPropertyName' was added with value: '$value'"];
         },
         'changed' => function ($astItem) use ($parents) {
             $fullPropertyName = $parents . $astItem['name'];
-            return ["Property '$fullPropertyName' was changed. From '" .
-             (is_bool($astItem['oldValue']) ? toBoolStr($astItem['oldValue']) : $astItem['oldValue']) . "' to '" .
-             (is_bool($astItem['newValue']) ? toBoolStr($astItem['newValue']) : $astItem['newValue']) . "'"];
+            $oldValue = valueToString($astItem['oldValue']);
+            $newValue = valueToString($astItem['newValue']);
+            return ["Property '$fullPropertyName' was changed. From '$oldValue' to '$newValue'"];
         },
         'need check in deep' => function ($astItem) use ($parents) {
             $nextParents = $parents . $astItem['name'] . '.';
             return renderToPlain($astItem['children'], $nextParents);
         }
     ];
-    return array_reduce($needNodes, function ($acc, $item) use ($typeMap) {
+    return array_reduce($neededNodes, function ($acc, $item) use ($typeMap) {
         $getNewItems = $typeMap[$item['type']];
         $newAcc = array_merge($acc, $getNewItems($item));
         return $newAcc;
